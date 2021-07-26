d088511119c4c00681cdaac3e284fb8f398b0f2f
Harvey Diaz <harbot421561@gmail.com>
2021-07-26 21:04:21 +0800

    Change sqlite to postgre

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ gravatar = Gravatar(app,
 
 ##CONNECT TO DB
 #sqlite:///blog.db
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
